33dd205feb5d49dbed23175a33b6b67c7295f9d4
NCSchmidt <nicklas.c.schmidt@gmail.com>
2018-06-16 14:04:22 -0700

    final kinda

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,25 @@ $(document).ready(function() {
     });
 
 
+    var randomIndexNumber = "";
+    var hangmanWord = "";
+
     var wordArray = ["skywalker", "ewoks", "jedi", "naboo", "leia", "luke", "han","jabba", "obiwan", "vader", "empire", "xwing", "tifighter","tatooine","wookie","chewbacca","finn","rey", "yoda","ben","anakin"];
     function setHangmanWord() {
-        var randomIndexNumber = Math.floor(Math.random()*wordArray.length);
-        var hangmanWord = wordArray[randomIndexNumber];
+        randomIndexNumber = Math.floor(Math.random()*wordArray.length);
+        hangmanWord = wordArray[randomIndexNumber];
         console.log(hangmanWord); // for DEV
     }
 
     setHangmanWord();
 
-    var keyTypedArray = [];
     var keyTyped = "";
+
+    var keyTypedArray = [];
+    var correctLetterArray = [];
+    var incorrectLetterArray = [];
+    var isLetterCorrect = false;
+
 
     function buildLetterArray(key) {
         keyTypedArray.push(key);
@@ -42,7 +50,36 @@ $(document).ready(function() {
         } else {
             // do a bunch of stuff
             buildLetterArray(keyTyped);
-            console.log(keyTypedArray);
+            console.log("key typed and lowercase: " + keyTypedArray);
+
+            //
+
+            for (n=0; n<hangmanWord.length; n++) { //go through computer generated word letters
+                //arrayAnswer
+                for (j=0; j<correctLetterArray.length; j++){ //go through array of correct chosed letters
+
+                    //if letter already chosen dont check again
+                    // else if letter not already chosen - compare letter with correct remaining letters
+                    if (keyTyped === hangmanWord[n]) { //if key typed add to correct letter array
+                        // put into correctLetterArray
+                        correctLetterArray.push(keyTyped);
+                        console.log("if is true and letter matches in computor arry:  " + correctLetterArray);
+                        return //stop once a correct letter is matched
+                        
+                    } else {
+                        // put into incorrectLetterArray
+                        incorrectLetterArray.push(keyTyped);
+                        console.log("else letter chosen not correct " + incorrectLetterArray);
+                    }
+                 }        
+                 
+                 //add code add in incorrect list or check hangmanword array again
+    
+            }
+            
+            console.log("after loop " + correctLetterArray); 
+
+            
         }
 
 
